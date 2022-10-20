3e0b14dc5140b163d15a2f26d022dfbc94aae9c5
bradyjhenry <me@bradyhenry.com>
2022-10-19 23:28:06 -0700

    README + Github Link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -121,7 +121,9 @@
 <footer class="page-footer font-small blue fixed-bottom">
 	<div class="footer-copyright text-center py-3">
 		Created by:
-		<a target="_blank" href="https://twitter.com/bradyjhenry"> Brady Henry</a>
+		<a target="_blank" rel="noreferrer" href="https://twitter.com/bradyjhenry"> Brady Henry</a>
+		-
+		<a target="_blank" rel="noreferrer" href="https://github.com/bradyjhenry/Halfbold"> Github</a>
 	</div>
 </footer>
 
